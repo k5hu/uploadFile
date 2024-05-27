c40d468272befda67e5dda56f54b38794f47ca87
Keyi Hu <keyihu@berkeley.edu>
2024-05-27 14:26:12 -0700

    upload to S3 and post to db test

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,11 +1,12 @@
 import React,{ useState } from "react";
 import {Button, FileInput, TextInput} from 'flowbite-react';
-import axios from 'axios';
 import {nanoid} from 'nanoid';
+import AWS, { LexRuntime } from 'aws-sdk';
+const s3 = new AWS.S3();
 
 export default function FormComponent() {
   const [input, setInput] = useState("");
-  const [file, setFile] = useState(null);
+  const [fileInput, setFile] = useState(null);
   // handle input change
   const handleInputChange = (e) => {
     setInput(e.target.value); // Update the text state
@@ -14,38 +15,65 @@ export default function FormComponent() {
 const handleFileChange = (e) => {
   setFile(e.target.files[0]); // Update the file state
 };
+
 // handle submit button, push all info to backend
 const handleSubmit = async(e) => {
   e.preventDefault();
-  const formData = new FormData();
-  formData.append('id', nanoid());
-  formData.append('text', input);
-  formData.append('file', file);
-  // use axios post request to send form data
-  try {
-    const response = await axios.post('/api/form', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    });
-    console.log("SUCCESS: ", response.data);
-  } catch (error) {
-    console.error("ERROR: Could not submit form");
-  };
+  // ----- get a presigned url from lambda -----
+  const file = fileInput.files[0];
+  const fileName = file.name;
+  const contentType = file.type;
+  const response = await fetch('https://7luh6x4oke.execute-api.us-west-2.amazonaws.com/test/generate-presigned-url?fileName=${encodeURIComponent(fileName)}&contentType=${encodeURIComponent(contentType)}');
+  const data = await response.json();
+  const url = data.url;
+
+  console.log(url)
+  // ----- put the file direclty to the s3 bucket with presigned url ----
+  const uploadResponse = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      
+    },
+    body: file,
+  });
+
+  // ---- store metadata in dynamodb ----
+  // 1. get bucket name and input_file_path
+  let urlObject = new URL(url);
+  const host = urlObject.host;
+  const parts = host.pathname.split('.');
+  const bucketName = parts[0];
+  const input_file_path = bucketName + '/' + fileName;
+
+  // 2. store metadata in dynamodb
+  const metadata = {
+    id: nanoid(),
+    input_text: input,
+    input_file_path: input_file_path,
+  }
+  const metadataResponse = await fetch('https://7luh6x4oke.execute-api.us-west-2.amazonaws.com/test/store-metadata', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(metadata),
+});
+
+  
 };
     return (
       <div className="min-h-screen bg-black flex ">
-        <form id = 'form'className="flex max-w-md flex-col gap-4 p-8 bg-black" onSubmit={handleSubmit}>
-            <div className = "container flex-row items-cente p-2">
-                <label htmlFor="text" className="text-white text-sm mb-2">TextInput:</label>
-                <TextInput id="input" type="text" className=" rounded bg-black border border-black"onChange={handleInputChange} />
+        <form id = 'form'className="p-4" onSubmit={handleSubmit}>
+            <div className = "container flex-row items-center mb-4">
+                <label htmlFor="text" className="block text-white mb-2">TextInput:</label>
+                <TextInput id="input" type="text" className="w-full" onChange={handleInputChange} />
             </div>
-            <div className = "container flex-row p-2">
-                <label htmlFor="file_upload" className="block text-white text-sm mb-2">FileInput: </label>
-                <FileInput id="file" className="rounded text-white bg-black border" onChange={handleFileChange} />
+            <div className = "container flex-row mb-4">
+                <label htmlFor="file_upload" className="block text-white mb-2">FileInput: </label>
+                <FileInput id="fileInput" className="w-full mb-2 " onChange={handleFileChange} />
             </div>
-            <div className="p-2">
-                <Button type="submit" className="border rounded ">Submit</Button>
+            <div>
+                <Button type="submit" className="border border-white text-white font-large me-2 mb-2 px-1.5 hover:bg-white hover:text-black w-1/5">Submit</Button>
             </div>
         </form>
       </div>
